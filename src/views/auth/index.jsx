5602071634a0abed92b3a51eaad7b145eb8848a5
hazemyoussif <37100042+hazemyoussif@users.noreply.github.com>
2022-06-03 09:50:22 +0200

    Build

--- src/views/auth/index.jsx
+++ src/views/auth/index.jsx
@@ -47,8 +47,8 @@ export const Auth = () => {
         * Enter any arbitrary username and password
       </div>
       <div className='bp3-text-muted bp3-text-small'>
-        * Build 1.30
-        * Build Time 2022-06-01 17:39
+        * Build 1.31
+        * Build Time 2022-06-03 08:49
       </div>
     </div>
   )
