e812e98978df84550f0a5ad5ce6416a35cebbbb4
hfyoussif <99352121+hfyoussif@users.noreply.github.com>
2023-02-14 10:46:45 +0200

    * ST Build 2.5.0

--- src/views/auth/index.jsx
+++ src/views/auth/index.jsx
@@ -46,8 +46,8 @@ export const Auth = () => {
         * Enter any arbitrary username and password
       </div> */}
       <div className='bp3-text-muted bp3-text-small'>
-        * ST Build 2.4.8
-        * Build Time 2022-02-08 11:20
+        * ST Build 2.4.9
+        * Build Time 2022-02-10 10:00
       </div>
     </div>
   )
