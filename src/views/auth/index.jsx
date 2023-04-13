178440b34602b9b26e7f69cf01a80a2f558bf1ab
hfyoussif <99352121+hfyoussif@users.noreply.github.com>
2023-04-13 13:00:56 +0200

    * QA Build 2.5.5

--- src/views/auth/index.jsx
+++ src/views/auth/index.jsx
@@ -1,43 +1,51 @@
-import { Button, Card, Classes, FormGroup, InputGroup } from '@blueprintjs/core'
-import { useCallback, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useRecoilState } from 'recoil'
-import { userState } from '../../store/user'
-import styles from './styles.module.scss'
-import logo from "../../images/logo.png"
+import {
+  Button,
+  Card,
+  Classes,
+  FormGroup,
+  InputGroup,
+} from "@blueprintjs/core";
+import { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../../store/user";
+import styles from "./styles.module.scss";
+import logo from "../../images/logo.png";
 
 export const Auth = () => {
-  const [user, setUser] = useRecoilState(userState)
-  const [password, setPassword] = useState(null)
-  const navigate = useNavigate()
+  const [user, setUser] = useRecoilState(userState);
+  const [password, setPassword] = useState(null);
+  const navigate = useNavigate();
 
   const onSubmit = useCallback(
-    e => {
-      e.preventDefault()
+    (e) => {
+      e.preventDefault();
 
-
-      navigate('/dashboard')
+      navigate("/dashboard");
     },
     [navigate]
-  )
+  );
 
   return (
     <div className={styles.container}>
-      <img  className={styles.logo} src={logo} alt="logo" />
+      <img className={styles.logo} src={logo} alt="logo" />
       <Card>
         <form className={styles.form} onSubmit={onSubmit}>
-          <FormGroup label='User' labelFor='user-input'>
-            <InputGroup id='user-input' onChange={event => setUser(event.target.value)} />
-          </FormGroup>
-          <FormGroup label='Password' labelFor='password-input'>
+          <FormGroup label="User" labelFor="user-input">
             <InputGroup
-              type='password'
-              id='password-input'
-              onChange={event => setPassword(event.target.value)}
+              id="user-input"
+              onChange={(event) => setUser(event.target.value)}
+            />
+          </FormGroup>
+          <FormGroup label="Password" labelFor="password-input">
+            <InputGroup
+              type="password"
+              id="password-input"
+              onChange={(event) => setPassword(event.target.value)}
             />
           </FormGroup>
 
-          <Button className={styles.submit} type='submit'>
+          <Button className={styles.submit} type="submit">
             Login
           </Button>
         </form>
@@ -45,10 +53,10 @@ export const Auth = () => {
       {/* <div className='bp3-text-muted bp3-text-small'>
         * Enter any arbitrary username and password
       </div> */}
-      <div className='bp3-text-muted bp3-text-small'>
-        * ST Build 2.5.1
+      <div className="bp3-text-muted bp3-text-small">
+        * QA Build 2.5.5
         * Build Time 2022-02-14 08:30
       </div>
     </div>
-  )
-}
+  );
+};
