18ad4d28597b515af5daff3e74f56adac82a0df1
hfyoussif <99352121+hfyoussif@users.noreply.github.com>
2023-04-13 13:00:24 +0200

    * QA Build 2.5.5

--- src/views/auth/index.jsx
+++ src/views/auth/index.jsx
@@ -46,7 +46,7 @@ export const Auth = () => {
         * Enter any arbitrary username and password
       </div> */}
       <div className='bp3-text-muted bp3-text-small'>
-        * QA Build 2.5.4
+        * QA Build 2.5.5
         * Build Time 2022-02-28 09:20
       </div>
     </div>
